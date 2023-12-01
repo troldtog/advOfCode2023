9390d7928ce002caa57666f2b5bcd98e2e9213ad
Tom Antonsen <44912232+troldtog@users.noreply.github.com>
2023-11-30 20:33:14 -0600

    dispatcher skeleton for each problem

--- bin/adv2023.dart
+++ bin/adv2023.dart
@@ -1,4 +1,5 @@
 import 'package:args/args.dart';
+import 'package:adv2023/prob1.dart' as prob1;
 
 const String version = '0.0.1';
 
@@ -20,7 +21,50 @@ ArgParser buildParser() {
       'version',
       negatable: false,
       help: 'Print the tool version.',
+    )
+    ..addOption(
+      'solveProblem',
+      abbr: 'p',
+      mandatory: true,
+      allowed: ['1'],
+      callback: solveProblem
     );
+}
+
+void solveProblem (String? problemNumber){
+  var result = "Result ${problemNumber}!";
+  switch (problemNumber){
+    case '1':
+      result = prob1.solve();
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '10':
+    case '11':
+    case '12':
+    case '13':
+    case '14':
+    case '15':
+    case '16':
+    case '17':
+    case '18':
+    case '19':
+    case '20':
+    case '21':
+    case '22':
+    case '23':
+    case '24':
+    case '25':
+    default:
+      // already covered by allowed above, but you can't tell solveProblem its argument is non-nullable.
+      throw FormatException('Invalid problem number. Problems are numbered 1 through 25.');
+  }
+  print(result);
 }
 
 void printUsage(ArgParser argParser) {
@@ -48,10 +92,12 @@ void main(List<String> arguments) {
     }
 
     // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
+    /* print('Positional arguments: ${results.rest}');
     if (verbose) {
       print('[VERBOSE] All arguments: ${results.arguments}');
     }
+    */
+
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
     print(e.message);
---
+++ lib/prob1.dart
@@ -0,0 +1,3 @@
+String solve(){
+    return "Solved!";
+}

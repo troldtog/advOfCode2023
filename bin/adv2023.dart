39385690f25d6424260daeb56d93e055222d9994
Tom Antonsen <44912232+troldtog@users.noreply.github.com>
2023-12-02 22:13:09 -0600

    Double count of problem numbers since there are two per day

--- bin/adv2023.dart
+++ bin/adv2023.dart
@@ -61,6 +61,32 @@ Future solveProblem (String? problemNumber, List<String> additionalArgs) async {
     case "23":
     case "24":
     case "25":
+    case "26":
+    case "27":
+    case "28":
+    case "29":
+    case "30":
+    case "31":
+    case "32":
+    case "33":
+    case "34":
+    case "35":
+    case "36":
+    case "37":
+    case "38":
+    case "39":
+    case "40":
+    case "41":
+    case "42":
+    case "43":
+    case "44":
+    case "45":
+    case "46":
+    case "47":
+    case "48":
+    case "49":
+    case "50":
+
     default:
       // WOULD BE NICE IF WE COULD VALIDATE THIS AS AN OPTION BUT WE CANNOT FOR SOME REASON?
       // I.E., allowed ["1","2"] recognizes problemToSolve=1 but errors on problemToSolve=2

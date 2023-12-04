2bdd39dfea2a5bd2742870ac8365bdc50ff8ef3f
Tom Antonsen <44912232+troldtog@users.noreply.github.com>
2023-12-03 19:16:30 -0600

    Day 3 Part 2

--- lib/day3.dart
+++ lib/day3.dart
@@ -48,19 +48,67 @@ Future<String> solvePart1(List<String> additionalArgs) async{
     return total.toString();
 }
 
-int sumAdjacentNumbers(int row, int col, List<List<String>> input){
-  int subtotal = 0;  
-  for ((int, int) neighbor in [(-1,-1), (-1,0), (-1,1), (0,-1),(0,1),(1,-1),(1,0),(1,1)]){
-    final (dx, dy) = neighbor;
-    var line = input[row+dy];
-    if (isDigit(line[col+dx])){
-      subtotal += extractNumber(line, col+dx);
+
+Future<String> solvePart2(List<String> additionalArgs) async{
+  final input = await readInputFile(additionalArgs[0]);
+  int total = 0;
+  for (var row = 1; row < input.length - 1; row++){
+    for (var col = 1; col < input[row].length - 1; col++){
+      if ("*" == input[row][col]){
+        total += computeGearRatio(row,col,input);
+      }
     }
   }
-  return subtotal;
+  return total.toString();
 }
 
-int extractNumber(List<String> line, int col){
+
+int computeGearRatio(int row, int col, List<List<String>> input){
+  final nearbyNumbers = findNearbyNumbers(row, col, input, false);
+  if (nearbyNumbers.length == 2){
+    return nearbyNumbers[0]*nearbyNumbers[1];
+  }
+
+  return 0;
+}
+
+int sumAdjacentNumbers(int row, int col, List<List<String>> input){
+  final nearbyNumbers = findNearbyNumbers(row, col, input, true);
+  nearbyNumbers.add(0);
+  return nearbyNumbers.reduce((value, number) => value + number);
+}
+
+List<int> findNearbyNumbers(int row, int col, List<List<String>> input, bool clearAfterFound){
+  List<int> foundNumbers = [];
+  foundNumbers.addAll(findNumbersOnAdjacentLine(input[row - 1], col, clearAfterFound));
+  foundNumbers.addAll(findNumbersOnSameLine(input[row], col, clearAfterFound));
+  foundNumbers.addAll(findNumbersOnAdjacentLine(input[row + 1], col, clearAfterFound));
+  return foundNumbers;
+}
+
+List<int> findNumbersOnAdjacentLine(List<String> line, int index, bool clearAfterFound){
+ if (isDigit(line[index])){
+   return [extractNumber(line, index, clearAfterFound)];
+ }
+
+ // misnomer, but once you identify that the spot directly above a symbol isn't a number the logic is the same.
+ return findNumbersOnSameLine(line, index, clearAfterFound);
+}
+
+List<int> findNumbersOnSameLine(List<String> line, int index, bool clearAfterFound){
+ List<int> adjacentResults = [];
+ if (isDigit(line[index - 1])){
+   adjacentResults.add(extractNumber(line, index - 1, clearAfterFound));
+ }
+
+ if (isDigit(line[index + 1])){
+   adjacentResults.add(extractNumber(line, index + 1, clearAfterFound));
+ }
+
+ return adjacentResults;
+}
+
+int extractNumber(List<String> line, int col, bool clearAfterFound){
   int numberIndexStart = col;
   int numberIndexEnd = col + 1;
   while (isDigit(line[numberIndexStart - 1])){
@@ -74,16 +122,12 @@ int extractNumber(List<String> line, int col){
   for (int i = numberIndexStart; i < numberIndexEnd; i++){
     foundNumber *= 10;
     foundNumber += int.parse(line[i]);
-
-    //numbers can be adjacent to multiple symbols so after it is read, set it to "." so it is not found twice.
-    line[i]=".";
+    
+    if (clearAfterFound){
+      //numbers can be adjacent to multiple symbols so after it is read, set it to "." so it is not found twice.
+      line[i]=".";
+    }
   }
   
-  print(foundNumber);
   return foundNumber;
-}
-
-
-Future<String> solvePart2(List<String> additionalArgs) async{
-    return "";
 }

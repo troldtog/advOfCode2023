22645f6497980474d464136a1b0d4b8db652dfed
Tom Antonsen <44912232+troldtog@users.noreply.github.com>
2023-12-06 20:36:07 -0600

    And comment

--- lib/day6.dart
+++ lib/day6.dart
@@ -1,23 +1,38 @@
-import 'util/fileIo.dart';
+import 'util/fileIo.dart' show readFileLineByLine;
+import 'package:collection/collection.dart' show IterableZip;
+import 'dart:math' show sqrt, pow;
 
 RegExp whitespace = RegExp(r"\s+");
-(num, num) toCenterAndDiscriminant(List<int> tuple) => (tuple[0]/2, sqrt(tuple[0]^2 - 4*tuple[1])/2);
-int ToStrictIntegerBound(num bound) => (bound.floor() == bound ? bound - 1 : bound).floor();
-(int, int) toBounds(List<num> tuple) => [-toStrictIntegerBound(tuple[1] - tuple[0]), toStrictIntegerBound(tuple[0] + tuple[1])];
-Stream<String> readFileData(string filePath) => readFileLineByLine().map((s) => line.split(":")[1].trim());
 
-Future<String> solvePart1(List<String> arguments){
-  final intGroups = readFileData(arguments[0]).map((s) => s.split(whitespace).map(int.parse));
+// takes race time and distance and returns the center and the discriminant of the parabola with zeros where result would tie the current
+// record and vertex at the maximum possible distance
+List<num> toCenterAndDiscriminant(List<int> tuple) => [tuple[0]/2, sqrt(pow(tuple[0],2) - 4*tuple[1])/2];
+
+// takes a number that is the least upper bound of an open interval and returns
+// the least upper bound of the largest closed interval with integer bounds
+int toStrictIntegerBound(num bound) => (bound.floor() == bound ? bound - 1 : bound).floor();
+
+// finds the least and greatest integer times that can beat the current record given the center (tuple[0])
+// and discriminant (tuple[1]) provided by toCenterAndDiscriminant.
+List<int> toBounds(List<num> tuple) => [-toStrictIntegerBound(tuple[1] - tuple[0]), toStrictIntegerBound(tuple[0] + tuple[1])];
+
+// chops off the boring parts of each line.
+Stream<String> readFileData(String filePath) => readFileLineByLine(filePath).map((s) => s.split(":")[1].trim());
+
+Future<String> solvePart1(List<String> arguments) async {
+  final raceInfo = readFileData(arguments[0]).map((s) => s.split(whitespace).map(int.parse));
+  return (await solve(raceInfo)).toString();
 }
 
-Future<String> solvePart2(List<String> arguments){
-  final intGroups = readFileData(arguments[0]).map((s) => [int.parse(s.replaceAll(whitespace, ""))]);
+Future<String> solvePart2(List<String> arguments) async {
+  final raceInfo = readFileData(arguments[0]).map((s) => [int.parse(s.replaceAll(whitespace, ""))]);
+  return (await solve(raceInfo)).toString();
 }
 
-Future<String> solve(Stream<Iterable<int>> fileLines){
+Future<int> solve(Stream<Iterable<int>> fileLines) async {
   final lines = await fileLines.toList();
-  answer = zip(lines).map(toCenterAndDiscriminant)
+  final answer = IterableZip<int>(lines).map(toCenterAndDiscriminant)
             .map(toBounds)
-            .reduce((ans, bounds) => ans*(bounds[1] - bounds[0] + 1));
+            .fold<int>(1, (ans, bounds) => ans*(bounds[1] - bounds[0] + 1));
   return answer;
 }
